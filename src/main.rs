80b2a476d0ca9e8c515aa101e41acad77658d1bb
Julius Kreuzer <jneuff@users.noreply.github.com>
2025-04-23 09:39:49 +0200

    Write first test (#2)
    * Write first test
    
    * Extract namespace creation
    
    ---------
    
    Co-authored-by: Julius Kreuzer <389874+jneuff@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod test {
+    use std::process::Command;
+
+    use k8s_openapi::api::core::v1::{Namespace, Secret};
+    use kube::{
+        Api, Client,
+        api::{ObjectMeta, PostParams},
+    };
+
+    async fn create_namespace(client: &Client, name: &str) -> Result<Namespace, kube::Error> {
+        let namespaces: Api<Namespace> = Api::all(client.clone());
+        namespaces
+            .create(
+                &PostParams::default(),
+                &Namespace {
+                    metadata: ObjectMeta {
+                        name: Some(name.to_string()),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+            )
+            .await
+    }
+
+    #[tokio::test]
+    async fn should_create_kwok_cluster() {
+        Command::new("kwokctl")
+            .args(["create", "cluster", "--name", "should_create_kwok_cluster"])
+            .output()
+            .unwrap();
+        let client = Client::try_default().await.unwrap();
+        create_namespace(&client, "new").await.unwrap();
+        let secrets: Api<Secret> = Api::namespaced(client, "new");
+        let new_secret = Secret {
+            metadata: ObjectMeta {
+                name: Some("new-secret".to_string()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        secrets
+            .create(&PostParams::default(), &new_secret)
+            .await
+            .unwrap();
+        let actual_secret = secrets.get("new-secret").await.unwrap();
+        assert_eq!(actual_secret.metadata.name, Some("new-secret".to_string()));
+        Command::new("kwokctl")
+            .args(["delete", "cluster", "--name", "should_create_kwok_cluster"])
+            .output()
+            .unwrap();
+    }
+}
