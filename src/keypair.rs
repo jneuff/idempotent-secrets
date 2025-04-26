4d15f06fa63a86043c8bd7be6821d5ce97772b30
Julius Kreuzer <389874+jneuff@users.noreply.github.com>
2025-04-26 21:36:35 +0200

    Render keys in PEM format

--- src/keypair.rs
+++ src/keypair.rs
@@ -1,12 +1,23 @@
 #[cfg(test)]
 mod tests {
-    use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+    use rsa::{
+        Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey,
+        pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey},
+        pkcs8::LineEnding,
+    };
 
     fn generate_keypair() -> Result<(RsaPrivateKey, RsaPublicKey), rsa::Error> {
         let mut rng = rand::thread_rng();
         let priv_key = RsaPrivateKey::new(&mut rng, 4096)?;
         let pub_key = RsaPublicKey::from(&priv_key);
         Ok((priv_key, pub_key))
+    }
+
+    fn generate_keypair_pem() -> Result<(String, String), rsa::Error> {
+        let (priv_key, pub_key) = generate_keypair()?;
+        let priv_pem = priv_key.to_pkcs1_pem(LineEnding::LF)?;
+        let pub_pem = pub_key.to_pkcs1_pem(LineEnding::LF)?;
+        Ok((priv_pem.to_string(), pub_pem.to_string()))
     }
 
     #[test]
@@ -25,5 +36,26 @@ mod tests {
             .decrypt(Pkcs1v15Encrypt, &enc_data)
             .expect("failed to decrypt");
         assert_eq!(&data[..], &dec_data[..]);
+    }
+
+    #[test]
+    fn should_render_pem() {
+        let (priv_pem, pub_pem) = generate_keypair_pem().unwrap();
+        assert_eq!(
+            priv_pem.lines().next().unwrap(),
+            "-----BEGIN RSA PRIVATE KEY-----"
+        );
+        assert_eq!(
+            priv_pem.lines().last().unwrap(),
+            "-----END RSA PRIVATE KEY-----"
+        );
+        assert_eq!(
+            pub_pem.lines().next().unwrap(),
+            "-----BEGIN RSA PUBLIC KEY-----"
+        );
+        assert_eq!(
+            pub_pem.lines().last().unwrap(),
+            "-----END RSA PUBLIC KEY-----"
+        );
     }
 }
