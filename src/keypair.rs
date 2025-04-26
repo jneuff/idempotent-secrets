46c1df6e9fecc7bfad2161186f2f4ff8c11b6085
Julius Kreuzer <389874+jneuff@users.noreply.github.com>
2025-04-26 19:24:38 +0200

    Extract function to generate keypair

--- src/keypair.rs
+++ src/keypair.rs
@@ -2,11 +2,17 @@
 mod tests {
     use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 
+    fn generate_keypair() -> Result<(RsaPrivateKey, RsaPublicKey), rsa::Error> {
+        let mut rng = rand::thread_rng();
+        let priv_key = RsaPrivateKey::new(&mut rng, 4096)?;
+        let pub_key = RsaPublicKey::from(&priv_key);
+        Ok((priv_key, pub_key))
+    }
+
     #[test]
     fn should_generate_keypair() {
         let mut rng = rand::thread_rng();
-        let priv_key = RsaPrivateKey::new(&mut rng, 4096).expect("failed to generate a key");
-        let pub_key = RsaPublicKey::from(&priv_key);
+        let (priv_key, pub_key) = generate_keypair().unwrap();
         // Encrypt
         let data = b"hello world";
         let enc_data = pub_key
