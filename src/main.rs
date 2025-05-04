3676b2bb1e1a17b868d26bcea74e6958bb8565f7
Julius Kreuzer <389874+jneuff@users.noreply.github.com>
2025-05-04 20:23:08 +0200

    Allow user to set namespace

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use clap::Parser;
 use std::collections::BTreeMap;
 
 mod k8s;
 mod keypair;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(long)]
+    namespace: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
     let (private_key, public_key) = keypair::generate_keypair_pem().unwrap();
     let data = BTreeMap::from([
@@ -17,7 +25,7 @@ async fn main() {
             k8s::ByteString(private_key.into_bytes()),
         ),
     ]);
-    k8s::create_secret("test-create-secret", "secret1", Some(data))
+    k8s::create_secret(&args.namespace, "secret1", Some(data))
         .await
         .unwrap();
 }
