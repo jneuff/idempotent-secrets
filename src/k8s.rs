f01bba21a0b3de9acd7b0a9a53f16687f0948d01
Julius Kreuzer <389874+jneuff@users.noreply.github.com>
2025-08-27 16:43:44 +0200

    chore: use secret as anchor in owner ref test

--- src/k8s.rs
+++ src/k8s.rs
@@ -52,20 +52,17 @@ pub async fn create_secret(
     name: &str,
     data: Option<BTreeMap<String, ByteString>>,
     owner_reference: Option<&OwnerReference>,
-) -> Result<(), kube::Error> {
+) -> Result<Secret, kube::Error> {
     let client = Client::try_default().await.unwrap();
     let secrets: Api<Secret> = Api::namespaced(client, namespace);
     let new_secret = secret(name, data, owner_reference);
-    secrets
-        .create(&PostParams::default(), &new_secret)
-        .await
-        .map(|_| ())
+    secrets.create(&PostParams::default(), &new_secret).await
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use k8s_openapi::api::core::v1::{ConfigMap, Namespace};
+    use k8s_openapi::api::core::v1::Namespace;
     use kube::api::ListParams;
 
     async fn create_namespace(client: &Client, name: &str) -> Result<Namespace, kube::Error> {
@@ -107,40 +104,19 @@ mod test {
         assert_eq!(actual, expected)
     }
 
-    async fn create_configmap(
-        client: &Client,
-        namespace: &str,
-        name: &str,
-    ) -> Result<ConfigMap, kube::Error> {
-        let configmaps: Api<ConfigMap> = Api::namespaced(client.clone(), namespace);
-        configmaps
-            .create(
-                &PostParams::default(),
-                &ConfigMap {
-                    metadata: ObjectMeta {
-                        name: Some(name.to_string()),
-                        ..Default::default()
-                    },
-                    data: None,
-                    ..Default::default()
-                },
-            )
-            .await
-    }
-
     #[tokio::test]
     async fn sets_owner_reference() {
         let client = Client::try_default().await.unwrap();
-        let namespace = "test-5";
+        let namespace = "test-sets-owner-reference";
         create_namespace(&client, namespace).await.unwrap();
-        let config_map = create_configmap(&client, namespace, "idempotent-secrets")
+        let secret = create_secret(namespace, "idempotent-secrets", None, None)
             .await
             .unwrap();
         let owner_reference = OwnerReference {
             api_version: "v1".to_string(),
-            kind: "ConfigMap".to_string(),
-            name: config_map.metadata.name.unwrap(),
-            uid: config_map.metadata.uid.unwrap(),
+            kind: "Secret".to_string(),
+            name: secret.metadata.name.unwrap(),
+            uid: secret.metadata.uid.unwrap(),
             ..Default::default()
         };
 
@@ -162,7 +138,7 @@ mod test {
 
         assert_eq!(owner_references.len(), 1);
         assert_eq!(owner_references[0].name, "idempotent-secrets");
-        assert_eq!(owner_references[0].kind, "ConfigMap");
+        assert_eq!(owner_references[0].kind, "Secret");
     }
 
     #[tokio::test]
